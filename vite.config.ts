215fdd21c62894064d9e6896cdf4a77c39954c25
toweringcloud <toweringcloud@gmail.com>
2023-11-11 00:37:14 +0900

    [MOD] project name change

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/xtwitter-clone",
+	base: "/twitter-clone",
 	build: { chunkSizeWarningLimit: 1024 },
 	plugins: [react()],
 });
